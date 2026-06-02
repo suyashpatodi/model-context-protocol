cf23acec5fce113482dbc2f27889c4e4e6744363
Suyash Patodi <suyashpatodijain@gmail.com>
2026-06-02 18:24:11 +0530

    MCP Client with local server for file system

---
+++ Mcp/Mcp.Client/Helper/Extension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Client;
+
+namespace Mcp.Client.LocalServer.Helper
+{
+    public static class Extension
+    {
+        public static async Task<IServiceCollection> AddSemanticKernelDependency(this IServiceCollection services, IConfiguration configuration)
+        {
+            var kernelBuilder = services.AddKernel();
+
+            await AddLocalServerTools(kernelBuilder);
+
+            var apiKey = configuration.GetValue<string>("github:apiKey") ?? string.Empty;
+            var model = configuration.GetValue<string>("github:model") ?? string.Empty;
+            var endpoint = configuration.GetValue<string>("github:endpoint") ?? string.Empty;
+
+            kernelBuilder.AddOpenAIChatCompletion(modelId: model, apiKey: apiKey, httpClient: new HttpClient { BaseAddress = new Uri(endpoint) });
+
+            FunctionChoiceBehaviorOptions options = new() { AllowConcurrentInvocation = true };
+
+            services.AddTransient<PromptExecutionSettings>(_ => new OpenAIPromptExecutionSettings()
+            {
+                Temperature = 0.9f,
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: options)
+            });
+
+            return services;
+        }
+
+        private static async Task AddLocalServerTools(IKernelBuilder kernelBuilder)
+        {
+            var options = new StdioClientTransportOptions()
+            {
+                Name = "File Sever",
+                Command = "npx",
+                Arguments = ["-y", "@modelcontextprotocol/server-filesystem", "D:\\Github\\model-context-protocol\\Mcp\\Mcp.Client\\Data\\"]
+            };
+
+            var mcpClient = await McpClient.CreateAsync(new StdioClientTransport(options));
+
+            IList<McpClientTool> tools = await mcpClient.ListToolsAsync();
+
+            var functions = tools.Select(x => x.AsKernelFunction());
+            kernelBuilder.Plugins.AddFromFunctions("FS", functions);
+        }
+    }
+}
